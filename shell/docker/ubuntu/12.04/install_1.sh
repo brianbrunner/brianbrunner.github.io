0010b96a97d147200d4ebfe7bcfa10295dc24940
brianbrunner <iam@brianbrunner.com>
2013-09-24 19:44:41 -0700

    REMOVED DUMB POST FUCK ME

--- shell/docker/ubuntu/12.04/install_1.sh
+++
@@ -1,3 +0,0 @@
-sudo apt-get update
-sudo apt-get install linux-image-generic-lts-raring linux-headers-generic-lts-raring
-sudo reboot
--- shell/docker/ubuntu/13.04/install.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get update
-sudo apt-get install linux-image-extra-`uname -r`
-sudo sh -c "curl https://get.docker.io/gpg | apt-key add -"
-sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
-sudo apt-get update
-sudo apt-get install -y lxc-docker
-sudo docker pull ubuntu
